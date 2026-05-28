87c9e74402ed546132c99af5d48f8e48c135a505
OthoC <alexandercantos161@gmail.com>
2026-05-27 23:30:18 -0500

    Add files via upload
    Actualización Main

--- src/main/java/com/example/uce/Main.java
+++ src/main/java/com/example/uce/Main.java
@@ -3,62 +3,68 @@ package com.example.uce;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-/* 
-        String birthday [] = new String[10];
-        birthday[0]="Jana Russell, March 12";
-        birthday[1]="London Chan, May 18";
-        birthday[7]="Rosie Mills, January 3";
 
-        String jana = birthday[0];
-        System.out.println(jana);
+        System.out.println("BINGO");
 
-        birthday[2]="8";
-        System.out.println(birthday);
+        String[][] bingo;
+        bingo = new String[5][5];
 
-        int tamanio = birthday.length;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < birthday.length; i++) {
-            System.out.println(birthday[1]);
-        }
-        do {
-            System.out.println(birthday[tamanio - 1]);
-            tamanio -- ;
-        }while (tamanio > 0);
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo.length; j++) {
+                int aux;
+                boolean repetido;
+                do {
+                    aux = (int) (Math.random() * 100);
+                    repetido = false;
 
-        int NewArray[]={1,2,3,5,8,13,21,34,55,89};
-        System.out.println(NewArray);
+                    for (int f = 0; f < bingo.length; f++) {
+                        for (int c = 0; c < bingo.length; c++) {
 
-    String Productos [] = {"Mouse " , "Teclado" , "Monitor" , "Tarj. Grafica" , "Cables" , "Cargadores"};3
+                            if (bingo[f][c] != null && bingo[f][c].equals(String.valueOf(aux))) {
+                                repetido = true;
+                            }
+                        }
+                    }
 
-    double Precio [] =  {5.15 , 8.92 , 114.15 , 668.91 , 3.00 , 22.00} ;
-    int Cantidad [] = new int[6];
-    double precio_cantidad [][] = new double [6][6] ;
-    Scanner sc = new Scanner (System.in);
-/* 
-    System.out.println("Ingrese La Cantidad De los Productos");
-    for (int i = 0; i < Precio.length; i++) {
-       System.out.println("Para "+Productos[i]+" con precio $"+Precio[i]);
-        String input = sc.nextLine();
-        if(input == "exit"){
-        System.exit(0);
-        }else{
-        Cantidad[i] = Integer.parseInt(input);
+                } while (repetido);
 
-               }
+                bingo[i][j] = String.valueOf(aux);
             }
-            for (int i = 0; i < Productos.length; i++) {
-                System.out.println("Productos:" + Productos[i] + 
-                    ", Precio: $" + Precio[i] + 
-                    ", Cantidad: " + Cantidad[i] );            
-                }
-        
-        for (int i = 0; i < Productos.length; i++) {
-            System.out.println("Ingrese el Precio Del Producto" + Productos[i] + " : ");
-            precio_cantidad[i][0] = sc.nextDouble() ;
-            System.out.println("Ingrese La Cantidad Del Producto" + Productos[i] + " : ");
-            precio_cantidad[i][1] = sc.nextDouble() ;
         }
-*/
+
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo.length; j++) {
+                System.out.print(bingo[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        while (true) {
+
+            System.out.println("Ingrese fila: ");
+            int fila = sc.nextInt();
+
+            System.out.println("Ingrese columna: ");
+            int columna = sc.nextInt();
+
+            if (fila < 0 || fila >= bingo.length ||
+                    columna < 0 || columna >= bingo[0].length) {
+
+                System.out.println("Posición inválida");
+                continue;
+            }
+            bingo[fila][columna] = "X";
+
+            for (int i = 0; i < bingo.length; i++) {
+                for (int j = 0; j < bingo.length; j++) {
+                    System.out.print(bingo[i][j] + "\t");
+                }
+                System.out.println();
+            }
         }
     }
+}
